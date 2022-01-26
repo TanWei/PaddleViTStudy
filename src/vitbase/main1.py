0e208f9a47ff0427f3d9e8e686ab9aefc035fc59
lei <dcsvgwudi@gmail.com>
2022-01-26 19:09:09 +0800

    vitbase

---
+++ src/vitbase/main1.py
@@ -0,0 +1,19 @@
+import paddle
+import paddle.nn as nn
+
+paddle.set_device('cpu')
+def main():
+    # 1. Create a tensor
+    t = paddle.zeros([3,3])
+    print(t)
+    # 2. Create a random tensor
+    # 3. Create a tensor from Image ./724/jpg 28x28
+    
+    # 4. print tensor tpye and dtype of tensor
+    
+    # 5. transpose imagetensor
+    # 6.Reshape a random int rwnsoe from  5x5 to 25
+
+
+if __name__ == "__main__":
+    main()
