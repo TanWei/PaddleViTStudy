5dfdda88fca9e62e956228df9f40fea13156d042
lei89 <dcsvgwudi@gmail.com>
2022-01-31 01:30:27 +0800

    20220131

---
+++ src/vitbase/attention.py
@@ -0,0 +1,73 @@
+from math import fabs
+from cv2 import transpose
+import paddle
+import paddle.nn as nn
+
+paddle.set_device('cpu')
+
+class Attention(nn.Layer):
+    def __init__(self, embed_dim, 
+                 num_heads, 
+                 qkv_bias=False, 
+                 qk_scale=None, 
+                 dropout=0.,
+                 attention_dropout=0.):
+        super().__init__()
+        self.num_heads = num_heads # 4
+        self.embed_dim = embed_dim # 96
+        self.head_dim = int(embed_dim / num_heads) #24
+        self.all_head_dim = self.head_dim * num_heads #96
+        self.qkv = nn.Linear(embed_dim,
+                             self.all_head_dim * 3,
+                             bias_attr=False if qkv_bias is False else None)
+        self.scale = self.head_dim ** -0.5 if qk_scale is None else qk_scale
+        self.softmax = nn.Softmax(-1)
+        self.proj = nn.Linear(self.all_head_dim, embed_dim)
+    def transpose_multi_head(self, x):
+        print(x.shape) # [8, 16, 96]
+        # x: [B, N, all_head_dim]
+        new_shape = x.shape[:-1] + [self.num_heads, self.head_dim]
+        x = x.reshape(new_shape)
+        print(x.shape) # [8, 16, 4, 24]
+        # x: [B, N, num_heads, head_dim]
+        x = x.transpose([0, 2, 1, 3])
+        # x: [B, num_heads, num_patches(N), head_dim] [8, ,4, 16， 24]
+        return x
+    
+    def forward(self, x):
+        B, N, _ = x.shape
+        qkvtmp = self.qkv(x)
+        print(qkvtmp.shape) # [8, 16, 96x3]
+        qkv = qkvtmp.chunk(3,-1)
+        print(type(qkv))
+        # [B, N, all_head_dim] * 3  
+        q, k, v = map(self.transpose_multi_head, qkv)
+        print(q.shape)
+        # [8, 4, 16, 24]
+        # q,k,v: [B, num_heads, num_patches, head_dim]
+        attn = paddle.matmul(q, k, transpose_y=True) # q * k'
+        attn = self.scale * attn
+        attn = self.softmax(attn)
+        attn_weights = attn
+        print(attn_weights.shape)
+        #dropout
+        # attn: [B, num_heads, num_patches, num_patches] 每个值对其他值的相似度矩阵
+
+        out = paddle.matmul(attn, v) # softmax(scale*(q*k')) * v
+        print(out.shape)
+        out = out.transpose([0,2,1,3])
+        # attn: [B, num_patches, num_heads, head_dim]
+        out = out.reshape([B, N, -1])
+        print(out.shape)
+        out = self.proj(out)
+        return out, attn_weights
+def main(): 
+    t = paddle.randn([8, 16, 96])
+    print(t)
+    # [4, 1024, 16] vit中的输入
+    model = Attention(embed_dim=96, num_heads=4, qkv_bias=False, qk_scale=None)
+    out, w = model(t)
+    print(out.shape)
+    print(w.shape)
+if __name__ == "__main__":
+    main()
--- src/vitbase/main1.py
+++ src/vitbase/main1.py
@@ -1,19 +1,28 @@
 import paddle
 import paddle.nn as nn
+from PIL import Image
+import numpy as np
 
 paddle.set_device('cpu')
 def main():
     # 1. Create a tensor
-    t = paddle.zeros([3,3])
-    print(t)
+    # t1 = paddle.zeros([3,3])
+    # print(t1)
     # 2. Create a random tensor
+    # t2 = paddle.randn([5,3])
+    # print(t2)
     # 3. Create a tensor from Image ./724/jpg 28x28
-    
+    img =   np.array(Image.open('./724.jpg'))
+    for i in range(28):
+        for j in range(28):
+            print(f'{img[i,j]:03} ', end='')
+        print()
     # 4. print tensor tpye and dtype of tensor
     
     # 5. transpose imagetensor
     # 6.Reshape a random int rwnsoe from  5x5 to 25
-
+    # 7.Unsqueeze a random int tensor from 5x5 to 5x5x1
+    # 8.chunk a random int tensor from 5x15 to 5x5, 5x5 and 5x5
 
 if __name__ == "__main__":
     main()
